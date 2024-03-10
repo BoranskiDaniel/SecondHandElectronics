cfdee7c49644576c685f43bcbd1b83b11969f1ac
Daniel Boranski <dboranski@gmail.com>
2024-03-10 14:49:58 +0200

    install mongoose and connect to the DB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
+const mongoose = require("mongoose");
 
-const { PORT } = require("./constants");
+const { PORT, DB_URL } = require("./constants");
 const routes = require("./router");
+const { log } = require("console");
 
 const app = express();
 
@@ -14,7 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 //Handlebars Configuration
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
-app.set("views", "src/views")
+app.set("views", "src/views");
+
+//DB Connection
+async function dbConnect() {
+    await mongoose.connect(DB_URL);
+}
+dbConnect()
+    .then(() => { console.log("Successfully connected to the DB!") })
+    .catch(err => console.log(`Error while connecting: ${err}`))
+
 
 // Routes
 app.use(routes)
