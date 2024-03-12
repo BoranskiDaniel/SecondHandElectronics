becc92f720cd3193575547b4804f161bf928d7c1
Daniel Boranski <dboranski@gmail.com>
2024-03-12 21:38:10 +0200

    hash password

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     password: { type: String, required: true },
 });
@@ -10,7 +11,12 @@ userSchema.virtual("rePassword").set(function (value) {
     if (value !== this.password) {
         throw new Error("Password missmatch!");
     }
-})
+});
+
+userSchema.pre("save", async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
