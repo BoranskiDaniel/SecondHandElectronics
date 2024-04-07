4e361b7044c2f50ca563b4b68ba62d2952fd4160
Daniel Boranski <dboranski@gmail.com>
2024-04-07 21:41:46 +0300

    promisify jsonwebtoken

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const CONFIG = {
     PORT: 5050,
-    DB_URL: "mongodb://127.0.0.1:27017/second-hand-electronics"
+    DB_URL: "mongodb://127.0.0.1:27017/second-hand-electronics",
+    SECRET: "138aa9e9-2604-41e8-8228-81d0a7df048e",
 }
 
 module.exports = CONFIG
